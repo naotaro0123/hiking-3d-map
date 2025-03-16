b9dc9537bbd90afc6a9f43a78ddaca79a07e5c47
naotaro0123 <n.ikewada@gmail.com>
2025-03-16 15:54:36 +0900

    fix: show map and lines

--- src/Components/MapLibreTerrain.tsx
+++ src/Components/MapLibreTerrain.tsx
@@ -1,7 +1,17 @@
+import mlcontour from "maplibre-contour";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import * as pmtiles from "pmtiles";
 import { useEffect, useRef } from "react";
+
+const demSource = new mlcontour.DemSource({
+  url: "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png",
+  encoding: "mapbox",
+  maxzoom: 12,
+  // offload contour line computation to a web worker
+  worker: true,
+});
+demSource.setupMaplibre(maplibregl);
 
 export const MapLibreTerrain = () => {
   const protocol = new pmtiles.Protocol();
@@ -15,10 +25,12 @@ export const MapLibreTerrain = () => {
 
     new maplibregl.Map({
       container,
-      center: [139.4894, 35.339],
-      zoom: 16,
+      center: [138.4339, 35.2139], // 富士山
+      maxPitch: 80,
+      zoom: 10,
       style: {
         version: 8,
+        glyphs: "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
         sources: {
           "background-osm-raster": {
             // ソースの種類。vector、raster、raster-dem、geojson、image、video のいずれか
@@ -56,6 +68,40 @@ export const MapLibreTerrain = () => {
             United Kingdom terrain data © Environment Agency copyright and/or database right 2015. All rights reserved;\
             United States 3DEP (formerly NED) and global GMTED2010 and SRTM terrain data courtesy of the U.S. Geological Survey.",
           },
+          // 標高タイルで等高線を描画する
+          hillshadeSource: {
+            type: "raster-dem",
+            // share cached raster-dem tiles with the contour source
+            tiles: [demSource.sharedDemProtocolUrl],
+            tileSize: 512,
+            maxzoom: 12,
+          },
+          // 等高線
+          contourSourceFeet: {
+            type: "vector",
+            tiles: [
+              demSource.contourProtocolUrl({
+                // meters to feet
+                multiplier: 3.28084,
+                overzoom: 1,
+                thresholds: {
+                  // zoom: [minor, major]
+                  //   major: 標高の値を表示するメジャーな等高線
+                  //   minor: 標高の値を表示しないマイナーな等高線
+                  //   zoomはズームレベルで上記値が適用される
+                  11: [1000, 1000],
+                  12: [500, 1000],
+                  13: [500, 1000],
+                  14: [200, 1000],
+                  15: [200, 1000],
+                },
+                elevationKey: "ele",
+                levelKey: "level",
+                contourLayer: "contours",
+              }),
+            ],
+            maxzoom: 15,
+          },
         },
         layers: [
           // 背景地図としてOpenStreetMapのラスタタイルを追加
@@ -73,10 +119,55 @@ export const MapLibreTerrain = () => {
             type: "hillshade",
             source: "aws-terriain",
           },
+          // 等高線
+          {
+            id: "hillshadeSource",
+            type: "hillshade",
+            source: "hillshadeSource",
+            layout: { visibility: "visible" },
+            paint: { "hillshade-exaggeration": 0.25 },
+          },
+          {
+            id: "contours",
+            type: "line",
+            source: "contourSourceFeet",
+            "source-layer": "contours",
+            paint: {
+              "line-opacity": 0.5,
+              // "major" contours have level=1, "minor" have level=0
+              "line-width": ["match", ["get", "level"], 1, 1, 0.5],
+            },
+          },
         ],
         terrain: {
           source: "aws-terriain",
           exaggeration: 1,
+        },
+        sky: {
+          // 空のベースカラー
+          "sky-color": "#199EF3",
+          // 空の色と水平線の色の混ぜ合わせ。1は空の真ん中の色を、0は空の色を使用する
+          "sky-horizon-blend": 0.5,
+          // 地平線のベースカラー
+          "horizon-color": "#ffffff",
+          // 霧の色と水平線の色の混ぜ合わせ。0は水平線の色、1は霧の色を使用する
+          "horizon-fog-blend": 0.5,
+          // 霧のベースカラー。 3D地形が必要
+          "fog-color": "#0000ff",
+          // 3D地形に霧を混ぜ合わせる。 0はマップの中心、1は地平線
+          "fog-ground-blend": 0.5,
+          // 大気の混ぜ合わせ。 1が可視大気、0が非表示大気
+          "atmosphere-blend": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            0,
+            1,
+            10,
+            1,
+            12,
+            0,
+          ],
         },
       },
     });
