f96216ce31d2a9702d146b396703ce00a1ec6d4b
naotaro0123 <n.ikewada@gmail.com>
2025-03-20 16:03:48 +0900

    fix: init params

--- src/map-styles/ui-style.ts
+++ src/map-styles/ui-style.ts
@@ -3,12 +3,16 @@ import "./ui-style.css";
 type InitViewSetting = {
   center: [number, number];
   maxPitch: number;
+  pitch: number;
+  bearing: number;
   zoom: number;
 };
 
 export const initViewSetting: InitViewSetting = {
   center: [138.4339, 35.2139], // 富士山
   maxPitch: 80,
+  pitch: 0,
+  bearing: 0,
   zoom: 10,
 };
 
